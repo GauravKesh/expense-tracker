568c3c20960b8c98fe348c6819ce1fd922d992ed
GauravKesh <gkrcoder@gmail.com>
2025-07-06 16:17:19 +0530

    fd

--- lib/piendpoint.ts
+++ lib/piendpoint.ts
@@ -1,4 +1,4 @@
-// const baseUrl = "https://personal-expense-backend.onrender.com/api"
-const baseUrl = "http://localhost:9090/api"
+const baseUrl = "https://personal-expense-backend.onrender.com/api"
+// const baseUrl = "http://localhost:9090/api"
 
 export default baseUrl;
